3193bb38285fb3ef7e0ff58cf3fb6b51ef2cf572
jbird55044 <jbird.m1@JamesDBird.com>
2020-12-10 11:41:26 -0600

    Updated to Add Store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getPizzas();
+  }
+
+  getPizzas = () => {
+    // tell axios to make a get call, then dispatch the array results
+    axios.get ('/api/pizza').then( (response) => {
+      console.log (`get`, response.data);
+      // dispatch Array results
+      this.props.dispatch ({type: 'GET_PIZZAS' , payload: response.data })
+
+    }).catch ( (err ) => {
+      console.log (`Error in Get`, err);
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -12,9 +31,13 @@ class App extends Component {
         <br/>
         <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
+        {JSON.stringify()}
       </div>
     );
   }
 }
 
-export default App;
+const putReduxStateOnProps = (reduxState) => ({ reduxState })
+export default connect(putReduxStateOnProps)(App);
+// export default App;
+ 
